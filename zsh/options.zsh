2c1a4504ecc772b91320e2297626686ca796565a
Brendan Loudermilk <brendan@gophilosophie.com>
2014-03-28 15:48:09 -0700

    [zsh] More personalization

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,1 +1,10 @@
-unsetopt share_history # Don't share command history data
+# Don't share command history data
+unsetopt share_history
+
+# Don't tell me what I meant
+unsetopt correct_all
+
+# Use C-x C-e to edit the current command line
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\C-x\C-e' edit-command-line
